eecb11654d015c0bff89aeb6bb07b24a363e8b8b
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2026-01-02 10:09:24 +0800

    leroux y xchire

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,8 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <Reminders />
             <Suspense fallback={null}>
+              <Reminders />
               <TransferAlertDialog />
               <ApproveDeletionDialog />
               <TicketEndorsed />
