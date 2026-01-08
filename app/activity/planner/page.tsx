1b375f99d5a61fe18868b374ba7ee9400b6b7fee
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2026-01-08 16:33:07 +0800

    Leroux Y Xchire

--- app/activity/planner/page.tsx
+++ app/activity/planner/page.tsx
@@ -269,7 +269,6 @@ function DashboardContent() {
                             <CardContent>
                                 <Completed
                                     referenceid={userDetails.referenceid}
-                                    target_quota={userDetails.target_quota}
                                     dateCreatedFilterRange={dateCreatedFilterRange}
                                     setDateCreatedFilterRangeAction={setDateCreatedFilterRangeAction} />
                             </CardContent>
