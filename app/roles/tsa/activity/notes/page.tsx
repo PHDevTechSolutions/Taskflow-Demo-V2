ee51eeb02fe0a96c54930a23308ce85ba3c6c9fb
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2026-02-20 13:55:59 +0800

    Leroux Y Xchire

--- app/roles/tsa/activity/notes/page.tsx
+++ app/roles/tsa/activity/notes/page.tsx
@@ -156,7 +156,7 @@ function DashboardContent() {
                                 tsm={userDetails.tsm}
                                 manager={userDetails.manager}
                                 dateCreatedFilterRange={dateCreatedFilterRange}
-                                setDateCreatedFilterRangeAction={setDateCreatedFilterRangeAction}
+                                
                             />
                         </div>
                     </main>
