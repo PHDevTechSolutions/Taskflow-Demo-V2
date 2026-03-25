7e25dab229734d0007bf2ba8e3a2632cc6bba5f1
ERPReactNextDev <it.ecoshiftcorp@gmail.com>
2026-03-25 18:40:56 +0800

    Leroux Y Xchire

--- app/roles/manager/reports/fb/page.tsx
+++ app/roles/manager/reports/fb/page.tsx
@@ -137,9 +137,9 @@ function DashboardContent() {
                         <div>
                             <FBTable
                                 referenceid={userDetails.referenceid}
-                                target_quota={userDetails.target_quota}
+
                                 dateCreatedFilterRange={dateCreatedFilterRange}
-                                setDateCreatedFilterRangeAction={setDateCreatedFilterRangeAction}
+
                                 userDetails={userDetails}
                             />
                         </div>
